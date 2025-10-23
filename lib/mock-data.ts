1395cc6056c7026286e74dec62d753f6fb9cba0a
Tarquin <3442322864@qq.com>
2025-10-23 16:38:38 +0800

    chore：添加假数据

---
+++ lib/mock-data.ts
@@ -0,0 +1,67 @@
+export interface Product {
+  id: string;
+  productName: string;
+  orderNumber: string;
+  itemNumber: string;
+  batch: string;
+  quantity: number;
+}
+
+export const mockProducts: Product[] = [
+  {
+    id: "1",
+    productName: "迪士尼狮子王辛巴",
+    orderNumber: "250905",
+    itemNumber: "HWJD1971",
+    batch: "第 21 批",
+    quantity: 3000,
+  },
+  {
+    id: "2",
+    productName: "迪士尼狮子王辛巴",
+    orderNumber: "250905",
+    itemNumber: "HWJD1971",
+    batch: "第 21 批",
+    quantity: 3000,
+  },
+  {
+    id: "3",
+    productName: "迪士尼狮子王辛巴",
+    orderNumber: "250905",
+    itemNumber: "HWJD1971",
+    batch: "第 21 批",
+    quantity: 3000,
+  },
+  {
+    id: "4",
+    productName: "迪士尼狮子王辛巴",
+    orderNumber: "250905",
+    itemNumber: "HWJD1971",
+    batch: "第 21 批",
+    quantity: 3000,
+  },
+  {
+    id: "5",
+    productName: "迪士尼狮子王辛巴",
+    orderNumber: "250905",
+    itemNumber: "HWJD1971",
+    batch: "第 21 批",
+    quantity: 3000,
+  },
+  {
+    id: "6",
+    productName: "迪士尼狮子王辛巴",
+    orderNumber: "250905",
+    itemNumber: "HWJD1971",
+    batch: "第 22 批",
+    quantity: 12000,
+  },
+  {
+    id: "7",
+    productName: "迪士尼狮子王辛巴",
+    orderNumber: "250905",
+    itemNumber: "HWJD1971",
+    batch: "第 22 批",
+    quantity: 3000,
+  },
+];
